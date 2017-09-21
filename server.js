2c8fceb04b53de1bf766093306d1f92424705b9b
sabithcad <sabith.cad@gmail.com>
2017-09-21 16:10:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,6 +83,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter', function(req,res){
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
 app.get('/:articleName', function(req, res){
     var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
@@ -92,6 +98,10 @@ app.get('/:articleName', function(req, res){
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
