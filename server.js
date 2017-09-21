9c590c561dcdeba9dbc07f526c4d640538715fd0
sabithcad <sabith.cad@gmail.com>
2017-09-21 11:17:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title : "article-one",
+    heading : "Article One",
+    date : "September 21, 2017"
+    content : `
+        <p>Hello this is article one. Hello this is article one. Hello this is article one. Hello this is article one. Hello this is article one. Hello this is article one.
+        </p>
+        <p>Hello this is article one. Hello this is article one. Hello this is article one. Hello this is article one. Hello this is article one. Hello this is article one.
+         </p>
+        <p>Hello this is article one. Hello this is article one. Hello this is article one. Hello this is article one. Hello this is article one. Hello this is article one.
+        </p>`
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
