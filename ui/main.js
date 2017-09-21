98e972f9befd321bfcd8cad9e867040335b489f0
sabithcad <sabith.cad@gmail.com>
2017-09-21 17:11:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,6 @@
 var button = document.getElementById("counter");
 var counter = 0;
 button.onClick = function(){
-   
-   console.log("TEsting");
    counter = counter + 1;
    var span = document.getElementById('count');
    span.innerHTML = counter.toString();
