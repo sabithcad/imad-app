f8f84c0d7f9a9211d1321309c738fc59680d8e4c
sabithcad <sabith.cad@gmail.com>
2017-09-24 11:17:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var Pool = require("pg").Pool;
 var config = {
   user:"sabithcad",
   database:"sabithcad",
-  host:"db.imad.hasura.io",
+  host:"db.imad.hasura-app.io",
   port:"5432",
   password:process.env.DB_PASSWORD
 };
