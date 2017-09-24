5ce79b9fae45918f05e8f4e1f6ea5a36b098f1a0
sabithcad <sabith.cad@gmail.com>
2017-09-24 21:30:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,12 +105,12 @@ app.get('/articles/:articleName', function(req, res){
 
 function hash(input,salt){
  var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    
+    return hashed.toString('hex');
 }
 
 
 app.get('/hash/:input', function(req,res){
-   var hashedString = hash(req.params.input, salt); 
+   var hashedString = hash(req.params.input, 'this-is-some-random-string'); 
 });
 
 app.get('/ui/style.css', function (req, res) {
