62a18cf75083a42b322966b7d98dde7874bd77e3
sabithcad <sabith.cad@gmail.com>
2017-09-25 12:41:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,6 +132,33 @@ app.post('/create-user', function(req,res){
    });
 });
 
+
+app.post('/login', function(req,res){
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   var hashed = hash(password , salt); 
+   pool.query('SELECT * from "user" where username = ($1)', [username], function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+                if(result.length === 0)
+                {
+                 res.status(403).send('username/password invalid');   
+                }
+                else
+                {
+                    var dbString = result.rows[0].password;
+                    var salt = dbString.split('$')[2];
+                
+                    res.send('correct credentials');
+                }
+            }
+   });
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
