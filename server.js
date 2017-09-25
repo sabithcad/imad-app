e06d6fb642e7752adf2ebc3afb40bb480506cc7a
sabithcad <sabith.cad@gmail.com>
2017-09-25 19:29:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,13 +114,6 @@ function hash(input,salt){
 }
 
 
-app.get('/hash/:input', function(req,res){
-   var hashedString = hash(req.params.input, 'this-is-some-random-string'); 
-   
-   res.send(hashedString);
-});
-
-
 app.post('/create-user', function(req,res){
    var username = req.body.username;
    var password = req.body.password;
